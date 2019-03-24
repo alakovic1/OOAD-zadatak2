f509a142cead91640ce55cdddfe0ed90a3cd98c4
alakovic1 <alakovic1@etf.unsa.ba>
2019-03-24 19:48:58 +0100

    zavrsen zadatak

---
+++ zadatak2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace zadatak2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Unesite broj n: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i <= n; i++)
+            {
+                for (int j = i; j < n; j++) Console.Write(" ");
+                for (int j = 0; j < 2 * i - 1; j++) Console.Write("*");
+                Console.Write("\n");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
